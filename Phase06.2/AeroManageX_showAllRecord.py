c96954b2301fa1307fce57463cba8107140cc4b7
mamcgahay <144842205+mamcgahay@users.noreply.github.com>
2023-11-29 18:17:45 -0500

    Add files via upload

---
+++ Phase06.2/AeroManageX_showAllRecord.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from tkinter.ttk import Treeview
+from tkinter import messagebox
+
+
+import mysql.connector
+
+
+mypass = "Savfish4$"
+mydatabase="mydb"
+con = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=mypass,
+        database=mydatabase
+    )
+cur = con.cursor()
+
+
+def showAll(userId):
+    class A(Frame):
+        def __init__(self, parent):
+            Frame.__init__(self, parent)
+            self.CreateUI()
+            self.LoadTable()
+            self.grid(sticky=(N, S, W, E))
+            parent.grid_rowconfigure(0, weight=1)
+            parent.grid_columnconfigure(0, weight=1)
+
+        def CreateUI(self):
+            tv = Treeview(self)
+            tv['columns'] = ('user_id', 'user_name', 'role_id')
+            tv.heading('#0', text='user_id', anchor='center')
+            tv.column('#0', anchor='center')
+            tv.heading('#1', text='user_name', anchor='center')
+            tv.column('#1', anchor='center')
+            tv.heading('#2', text='role_id', anchor='center')
+            tv.column('#2', anchor='center')
+            tv.grid(sticky=(N, S, W, E))
+            self.treeview = tv
+            self.grid_rowconfigure(0, weight=1)
+            self.grid_columnconfigure(0, weight=1)
+
+        def LoadTable(self):
+            cur.execute("SELECT * FROM usercontacts WHERE user_id = %s", (userId,))
+            result = cur.fetchall()
+            user_id = ""
+            user_name = ""
+            role_id = ""
+            for i in result:
+                user_id = i[0]
+                user_name = i[1]
+                role_id = i[2]
+                self.treeview.insert("", 'end', text=user_id,
+                             values=(user_name, role_id))
+    root = Tk()
+    root.title("Your contacts")
+    A(root)
